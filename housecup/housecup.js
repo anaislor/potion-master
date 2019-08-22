bf87c156045ceca557ac3f1e6125152b0440a655
Anaïs Lorteau <anaislor@orange.fr>
2019-08-22 19:15:56 +0200

    fix bug

--- housecup/housecup.js
+++ housecup/housecup.js
@@ -1,171 +1,178 @@
-class Character{
-  constructor(name, image, html,nb){
-    this.name= name;
-    this.image= image;
-    this.totalscore= 0;
+class Character {
+  constructor(name, image, html, nb) {
+    this.name = name;
+    this.image = image;
+    this.totalscore = 0;
   }
 }
 
 var gryffindor = new Character("gryffindor", "/images/gryffindor.png");
 var slytherin = new Character("slytherin", "/images/slytherin.png");
-var hufflepuff= new Character("hufflepuff","/images/hufflepuff.png");
-var ravenclaw = new Character("ravenclaw","/images/ravenclaw.png");
+var hufflepuff = new Character("hufflepuff", "/images/hufflepuff.png");
+var ravenclaw = new Character("ravenclaw", "/images/ravenclaw.png");
 
-class Game{
-  constructor(){
+class Game {
+  constructor() {
     this.answer = [];
-    this.answerImage=[]
+    this.answerImage = [];
     this.triesList = 0;
     this.try = [];
-    this.player=[];
+    this.player = [];
     this.score = 120;
   }
 
+  randomAnswer() {
+    let proposition = [
+      "Flies",
+      "Leeches",
+      "Bicorn Horn",
+      "Boomslang Skin",
+      "Love Philter",
+      "Knotgrass"
+    ];
+    let imageProposition = [
+      "/images/flies.png",
+      "/images/leeches.png",
+      "/images/bicorn-horn.png",
+      "/images/boomslang-skin.png",
+      "/images/elixir.png",
+      "/images/plant.png"
+    ];
 
-  randomAnswer(){
-    let proposition =["Flies", "Leeches", "Bicorn Horn", "Boomslang Skin"];
-    let imageProposition =["/images/flies.png", "/images/leeches.png","/images/bicorn-horn.png","/images/boomslang-skin.png"]
-
-    for(let i = 0; i < 4; i++){
-    let random = Math.floor(Math.random() * 4);
-    this.answer.push(proposition[random])
-    this.answerImage.push(imageProposition[random])
+    for (let i = 0; i < 4; i++) {
+      let random = Math.floor(Math.random() * 6);
+      this.answer.push(proposition[random]);
+      this.answerImage.push(imageProposition[random]);
     }
-
   }
 
-  arrayEqual(){
-    let sum = 0
-    for(let i=0; i < this.answer.length; i++){
-      if(this.try[i] == this.answer[i]){
-        sum +=1
+  arrayEqual() {
+    let sum = 0;
+    for (let i = 0; i < this.answer.length; i++) {
+      if (this.try[i] == this.answer[i]) {
+        sum += 1;
       }
     }
-      if(sum === 4){
-        return true
-      }
-      else{return false}
+    if (sum === 4) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-
-  checkIf(){
-    if(this.arrayEqual() === true){
-      this.scoreAdded()
-      showPopupWinnerCup()
-    }
-    else{
-      //vérifier si l'ingrédient est présent et à la bonne place
-      for(let i=0; i < this.answer.length; i++){
-        let answerFilter= this.answer.filter(ingredient => ingredient === this.try[i])
-        let tryFilter= this.try.filter(ingredient => ingredient === this.try[i]);
-        if(this.try[i] === this.answer[i]){
-          console.log(`${this.try[i]} is in the array at the good place`)
-          let helpGreen= document.querySelector(".try:last-child")
-          helpGreen.innerHTML += `<img class="help" src="/images/green.png">`
+  checkIf() {
+    for (let i = 0; i < this.try.length; i++) {
+      if (this.answer.includes(this.try[i])) {
+        //vérifier si l'ingrédient est présent et à la bonne place
+        if (this.try[i] === this.answer[i]) {
+          console.log(`${this.try[i]} is in the array at the good place`);
+          let helpGreen = document.querySelector(".try:last-child");
+          helpGreen.innerHTML += `<img class="help" src="/images/green.png">`;
         }
-        
         //vérifier si l'ingrédient est présent mais pas à la bonne place
-        else if(tryFilter.length <= answerFilter.length && i-1 <= answerFilter.length){
-          console.log(`${this.try[i]} is in the answer but not at the good place`)
-          let helpRed= document.querySelector(".try:last-child")
-          helpRed.innerHTML += `<img class="help" src="/images/red.png">`
-        }
-        //l'ingrédient n'est pas présent
-        else{
-          console.log(`${this.try[i]} not found in the answer`)
-
+        else {
+          console.log(
+            `${this.try[i]} is in the answer but not at the good place`
+          );
+          let helpRed = document.querySelector(".try:last-child");
+          helpRed.innerHTML += `<img class="help" src="/images/red.png">`;
         }
       }
-      this.scorecalculation()
+      //ingrédient n'existe pas
+      else {
+        console.log(`${this.try[i]} not found in the answer`);
+      }
     }
-    this.triesList +=1
-    if(this.triesList === 12){
-      this.finishGame()
+    if (this.arrayEqual() === true) {
+      this.scoreAdded();
+      showPopupWinnerCup();
     }
-    this.emptyArray()
+    this.scorecalculation();
+    this.triesList += 1;
+    if (this.triesList === 12) {
+      this.finishGame();
+    }
+    this.emptyArray();
   }
 
-  arrayFull(){
-    if(this.try.length === 4){
-      this.checkIf()
-      return true
+  arrayFull() {
+    if (this.try.length === 4) {
+      this.checkIf();
+      return true;
+    } else {
+      return false;
     }
-    else{
-      return false
-    }
   }
 
-  scorecalculation(){
-    this.score-=10;
-    console.log(this.score)
+  scorecalculation() {
+    this.score -= 10;
+    console.log(this.score);
   }
 
-  emptyArray(){
-    return this.try.splice(0,4)
+  emptyArray() {
+    return this.try.splice(0, 4);
   }
 
-  scoreAdded(){
-    if(this.player.indexOf("gryffindor")===0){
+  scoreAdded() {
+    if (this.player.indexOf("gryffindor") === 0) {
       gryffindor.totalscore = this.score;
-      localStorage.setItem("gryffindor", this.score)
-    }
-    else if(this.player.indexOf("slytherin")===0){
+      localStorage.setItem("gryffindor", this.score);
+    } else if (this.player.indexOf("slytherin") === 0) {
       slytherin.totalscore = this.score;
-      localStorage.setItem("slytherin", this.score)
-
-    }
-    else if(this.player.indexOf("ravenclaw")===0){
+      localStorage.setItem("slytherin", this.score);
+    } else if (this.player.indexOf("ravenclaw") === 0) {
       ravenclaw.totalscore = this.score;
-      localStorage.setItem("ravenclaw", this.score)
-    }
-    else if(this.player.indexOf("hufflepuff")===0){
+      localStorage.setItem("ravenclaw", this.score);
+    } else if (this.player.indexOf("hufflepuff") === 0) {
       hufflepuff.totalscore = this.score;
-      localStorage.setItem("hufflepuff", this.score)
-      }
-  }   
-
-  finishGame(){
-    if(this.arrayEqual() === true){
-      this.scoreAdded()
-      showPopupWinnerCup()
+      localStorage.setItem("hufflepuff", this.score);
     }
-    else{
-      this.scoreAdded()
-      showPopupLooserCup()
+  }
+
+  finishGame() {
+    if (this.arrayEqual() === true) {
+      this.scoreAdded();
+      showPopupWinnerCup();
+    } else {
+      this.scoreAdded();
+      showPopupLooserCup();
     }
   }
 }
 
-let potionMaster = new Game()
+let potionMaster = new Game();
 
-console.log(potionMaster.player)
+console.log(potionMaster.player);
 
 let popup;
 
 window.addEventListener("DOMContentLoaded", function() {
   popup = document.querySelector(".popup");
-  let title = document.title
+  let title = document.title;
   potionMaster.player.push(title);
 });
 
-class Ingredient{
-  constructor(name, image){
-    this.name= name;
-    this.image= image;
+class Ingredient {
+  constructor(name, image) {
+    this.name = name;
+    this.image = image;
   }
 
-  addToTry(){
-    potionMaster.try.push(this.name)
-    console.log(`${this.name} add to try`)
+  addToTry() {
+    potionMaster.try.push(this.name);
+    console.log(`${this.name} add to try`);
   }
 }
 
 var flies = new Ingredient("Flies", "/images/flies.png");
-var leeches = new Ingredient("Leeches","/images/leeches.png");
+var leeches = new Ingredient("Leeches", "/images/leeches.png");
 var bicornHorn = new Ingredient("Bicorn Horn", "/images/bicorn-horn.png");
-var boomslangSkin = new Ingredient("Boomslang Skin","/images/boomslang-skin.png")
-
-
+var boomslangSkin = new Ingredient(
+  "Boomslang Skin",
+  "/images/boomslang-skin.png"
+);
+var lovePhilter = new Ingredient("Love Philter", "/images/elixir.png");
+var knotgrass = new Ingredient("Knotgrass", "/images/plant.png");
 
 //afficher les alertes
 function showPopupLooserCup() {
@@ -181,8 +188,7 @@ function showPopupLooserCup() {
   <img class="answer" src="${potionMaster.answerImage[3]}">
   </div>
   <a class="btn" href="/housecup/score.html">Score</a>
-  </div>`
-
+  </div>`;
 }
 
 function showPopupWinnerCup() {
@@ -191,6 +197,5 @@ function showPopupWinnerCup() {
   <h3>Congratulation!</h3>
   <img src="/images/winner.gif" alt="winner">
   <a class="btn" href="/housecup/score.html">Score</a>
-  </div>`
-
+  </div>`;
 }
--- js/game.js
+++ js/game.js
@@ -1,141 +1,136 @@
-
-class Game{
-  constructor(){
+class Game {
+  constructor() {
     this.answer = [];
-    this.answerImage=[]
+    this.answerImage = [];
     this.triesList = 0;
     this.try = [];
-    this.players=[];
+    this.players = [];
   }
 
+  randomAnswer() {
+    let proposition = [
+      "Flies",
+      "Leeches",
+      "Bicorn Horn",
+      "Boomslang Skin",
+      "Love Philter",
+      "Knotgrass"
+    ];
+    let imageProposition = [
+      "/images/flies.png",
+      "/images/leeches.png",
+      "/images/bicorn-horn.png",
+      "/images/boomslang-skin.png",
+      "/images/elixir.png",
+      "/images/plant.png"
+    ];
 
-  randomAnswer(){
-    let proposition =["Flies", "Leeches", "Bicorn Horn", "Boomslang Skin", "Love Philter", "Knotgrass"];
-    let imageProposition =["/images/flies.png", "/images/leeches.png","/images/bicorn-horn.png","/images/boomslang-skin.png","/images/elixir.png","/images/plant.png"]
-
-    for(let i = 0; i < 4; i++){
-    let random = Math.floor(Math.random() * 6);
-    this.answer.push(proposition[random])
-    this.answerImage.push(imageProposition[random])
+    for (let i = 0; i < 4; i++) {
+      let random = Math.floor(Math.random() * 6);
+      this.answer.push(proposition[random]);
+      this.answerImage.push(imageProposition[random]);
     }
-
   }
 
-  arrayEqual(){
-    let sum = 0
-    for(let i=0; i < this.answer.length; i++){
-      if(this.try[i] == this.answer[i]){
-        sum +=1
+  arrayEqual() {
+    let sum = 0;
+    for (let i = 0; i < this.answer.length; i++) {
+      if (this.try[i] == this.answer[i]) {
+        sum += 1;
       }
     }
-      if(sum === 4){
-        return true
-      }
-      else{return false}
+    if (sum === 4) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-
-  checkIf(){
-    if(this.arrayEqual() === true){
-      showPopupWinner()
-    }
-    else{
-      //vérifier si l'ingrédient est présent et à la bonne place
-      for(let i=0; i < this.answer.length; i++){
-        let answerFilter= this.answer.filter(ingredient => ingredient === this.try[i])
-        let tryFilter= this.try.filter(ingredient => ingredient === this.try[i]);
-        if(this.try[i] === this.answer[i]){
-          console.log(`${this.try[i]} is in the array at the good place`)
-          let helpGreen= document.querySelector(".try:last-child")
-          helpGreen.innerHTML += `<img class="help" src="/images/green.png">`
+  checkIf() {
+    for (let i = 0; i < this.try.length; i++) {
+      if (this.answer.includes(this.try[i])) {
+        //vérifier si l'ingrédient est présent et à la bonne place
+        if (this.try[i] === this.answer[i]) {
+          console.log(`${this.try[i]} is in the array at the good place`);
+          let helpGreen = document.querySelector(".try:last-child");
+          helpGreen.innerHTML += `<img class="help" src="/images/green.png">`;
         }
-        
         //vérifier si l'ingrédient est présent mais pas à la bonne place
-        else if(tryFilter.length <= answerFilter.length && i-1 <= answerFilter.length){
-          console.log(`${this.try[i]} is in the answer but not at the good place`)
-          let helpRed= document.querySelector(".try:last-child")
-          helpRed.innerHTML += `<img class="help" src="/images/red.png">`
-        }
-        //l'ingrédient n'est pas présent
-        else{
-          console.log(`${this.try[i]} not found in the answer`)
-
+        else {
+          console.log(
+            `${this.try[i]} is in the answer but not at the good place`
+          );
+          let helpRed = document.querySelector(".try:last-child");
+          helpRed.innerHTML += `<img class="help" src="/images/red.png">`;
         }
       }
-      if(Character.name === "gryffindor"){
-        gryffindor.score -= 10;
-        console.log(gryffindor.score)
-      }
-      else if(Character.name === "slytherin"){
-        slytherin.score -= 10;
-        console.log(slytherin.score);
-      }
-      else if(Character.name === "ravenclaw"){
-        ravenclaw.score -= 10;
-        console.log(ravenclaw.score);
-      }
+      //ingrédient n'existe pas
       else {
-        hufflepuff.score -= 10;
-        console.log(hufflepuff.score);
+        console.log(`${this.try[i]} not found in the answer`);
       }
     }
-    this.triesList +=1
-    if(this.triesList === 12){
-      this.finishGame()
+    if (this.arrayEqual() === true) {
+      this.scoreAdded();
+      showPopupWinnerCup();
     }
-    this.emptyArray()
+    this.triesList += 1;
+
+    if (this.triesList === 12) {
+      this.finishGame();
+    }
+    this.emptyArray();
   }
 
-  arrayFull(){
-    if(this.try.length === 4){
-      this.checkIf()
-      return true
-    }
-    else{
-      return false
+  arrayFull() {
+    if (this.try.length === 4) {
+      this.checkIf();
+      return true;
+    } else {
+      return false;
     }
   }
 
-  emptyArray(){
-    return this.try.splice(0,4)
+  emptyArray() {
+    return this.try.splice(0, 4);
   }
 
-  finishGame(){
-      if(this.arrayEqual() === true){
-        showPopupWinner()
-      }
-      else{
-        showPopupLooser()
-      }
+  finishGame() {
+    if (this.arrayEqual() === true) {
+      showPopupWinner();
+    } else {
+      showPopupLooser();
+    }
   }
 }
 
-let potionMaster = new Game()
+let potionMaster = new Game();
 
-class Ingredient{
-  constructor(name, image){
-    this.name= name;
-    this.image= image;
+class Ingredient {
+  constructor(name, image) {
+    this.name = name;
+    this.image = image;
   }
 
-  addToTry(){
-    potionMaster.try.push(this.name)
-    console.log(`${this.name} add to try`)
+  addToTry() {
+    potionMaster.try.push(this.name);
+    console.log(`${this.name} add to try`);
   }
 }
 
 var flies = new Ingredient("Flies", "/images/flies.png");
-var leeches = new Ingredient("Leeches","/images/leeches.png");
+var leeches = new Ingredient("Leeches", "/images/leeches.png");
 var bicornHorn = new Ingredient("Bicorn Horn", "/images/bicorn-horn.png");
-var boomslangSkin = new Ingredient("Boomslang Skin","/images/boomslang-skin.png")
+var boomslangSkin = new Ingredient(
+  "Boomslang Skin",
+  "/images/boomslang-skin.png"
+);
 var lovePhilter = new Ingredient("Love Philter", "/images/elixir.png");
-var knotgrass = new Ingredient("Knotgrass","/images/plant.png");
+var knotgrass = new Ingredient("Knotgrass", "/images/plant.png");
 var popup;
 
 window.addEventListener("DOMContentLoaded", function() {
-    popup = document.getElementById("popup_1");
+  popup = document.getElementById("popup_1");
 });
-
 
 //afficher les alertes
 function showPopupLooser() {
@@ -154,8 +149,7 @@ function showPopupLooser() {
   <button class="btn" onclick="location.reload()">Play again !</button>
   <a class="btn" href="/index.html">Menu</a>
   </div>
-  </div>`
-
+  </div>`;
 }
 
 function showPopupWinner() {
@@ -167,23 +161,38 @@ function showPopupWinner() {
   <button class="btn" onclick="location.reload()">Play again !</button>
   <a class="btn" href="/index.html">Menu</a>
   </div>
-  </div>`
-
+  </div>`;
 }
 
-class Character{
-  constructor(name, image, html){
-    this.name= name;
-    this.image= image;
-    this.score= 120;
-    this.html= html
+class Character {
+  constructor(name, image, html) {
+    this.name = name;
+    this.image = image;
+    this.score = 120;
+    this.html = html;
   }
 }
 
-var gryffindor = new Character("gryffindor", "/images/gryffindor.png","/html/gryffindor.html");
-var slytherin = new Character("slytherin", "/images/slytherin.png","/html/slytherin.html");
-var hufflepuff= new Character("hufflepuff","/images/hufflepuff.png","/html/hufflepuff.html");
-var ravenclaw = new Character("ravenclaw","/images/ravenclaw.png","/html/ravenclaw.html")
+var gryffindor = new Character(
+  "gryffindor",
+  "/images/gryffindor.png",
+  "/html/gryffindor.html"
+);
+var slytherin = new Character(
+  "slytherin",
+  "/images/slytherin.png",
+  "/html/slytherin.html"
+);
+var hufflepuff = new Character(
+  "hufflepuff",
+  "/images/hufflepuff.png",
+  "/html/hufflepuff.html"
+);
+var ravenclaw = new Character(
+  "ravenclaw",
+  "/images/ravenclaw.png",
+  "/html/ravenclaw.html"
+);
 
 // house cup
 
@@ -200,8 +209,7 @@ function showPopupLooserCup() {
   <img class="answer" src="${potionMaster.answerImage[3]}">
   </div>
   <a class="btn" href="/html/score.html">Score !</a>
-  </div>`
-
+  </div>`;
 }
 
 function showPopupWinnerCup() {
@@ -210,7 +218,5 @@ function showPopupWinnerCup() {
   <h3>100 points for Gryffondor !</h3>
   <img src="/images/winner.gif" alt="winner">
   <a class="btn" href="/html/score.html">Score !</a>
-  </div>`
-
+  </div>`;
 }
-
